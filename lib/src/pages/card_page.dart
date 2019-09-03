1c2d6f92de6ab978f57487480509f9744aa4bde9
mmunozpatino <mercedesmp1305@gmail.com>
2019-09-02 23:37:16 -0300

    video 74 OK

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,40 +9,73 @@ class CardPage extends StatelessWidget {
         ),
         body: ListView(
           padding: EdgeInsets.all(10.0),
-          children: <Widget>[_cardTipo1()],
+          children: <Widget>[
+            _cardTipo1(),
+            SizedBox(
+              height: 30.0,
+            ),
+            _cardTipo2()
+          ],
         ));
   }
 
   Widget _cardTipo1() {
-
     return Card(
       child: Column(
         children: <Widget>[
           ListTile(
             leading: Icon(Icons.photo_album, color: Colors.deepPurple),
             title: Text('Soy el titulo de esta tarjeta'),
-            subtitle: Text('esta es la descripción hiper larga de la tarjeta para ver que quiere mostranos el colombiano'),
+            subtitle: Text(
+                'esta es la descripción hiper larga de la tarjeta para ver que quiere mostranos el colombiano'),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               FlatButton(
                 child: Text('Cancelar'),
-                onPressed: (){},
+                onPressed: () {},
                 textColor: Colors.deepPurple,
               ),
-              SizedBox(width: 10,),
+              SizedBox(
+                width: 10,
+              ),
               FlatButton(
                 child: Text('Aceptar'),
-                onPressed: (){},
+                onPressed: () {},
                 color: Colors.deepPurple,
                 textColor: Colors.white,
               ),
-              SizedBox(width: 10,)
+              SizedBox(
+                width: 10,
+              )
             ],
-            )
+          )
         ],
       ),
+    );
+  }
+
+  _cardTipo2() {
+    return Card(
+        child: Column(
+          children: <Widget>[
+            FadeInImage(
+              placeholder: AssetImage('assets/jar-loading.gif'),
+              image: NetworkImage('https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg'),
+              fadeInDuration: Duration( microseconds: 100),
+              height: 300.0,
+              fit: BoxFit.cover
+            ),
+            // Image(
+            //   image: NetworkImage('https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg')
+            // ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('No tengo idea de que poner')
+              )
+          ],
+        ),
     );
   }
 }
