5500e725f21a99aec9fc688d87267e632faf6862
mmunozpatino <mercedesmp1305@gmail.com>
2019-09-03 13:53:50 -0300

    video 75 OK

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -14,13 +14,31 @@ class CardPage extends StatelessWidget {
             SizedBox(
               height: 30.0,
             ),
-            _cardTipo2()
+            _cardTipo2(),
+            SizedBox(
+              height: 30.0,
+            ),
+            _cardTipo1(),
+            SizedBox(
+              height: 30.0,
+            ),
+            _cardTipo2(),
+            SizedBox(
+              height: 30.0,
+            ),
+            _cardTipo1(),
+            SizedBox(
+              height: 30.0,
+            ),
+            _cardTipo2(),
           ],
         ));
   }
 
   Widget _cardTipo1() {
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         children: <Widget>[
           ListTile(
@@ -57,25 +75,45 @@ class CardPage extends StatelessWidget {
   }
 
   _cardTipo2() {
-    return Card(
-        child: Column(
-          children: <Widget>[
-            FadeInImage(
+    final card = Container(
+      // clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
               placeholder: AssetImage('assets/jar-loading.gif'),
-              image: NetworkImage('https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg'),
-              fadeInDuration: Duration( microseconds: 100),
+              image: NetworkImage(
+                  'https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg'),
+              fadeInDuration: Duration(microseconds: 100),
               height: 300.0,
-              fit: BoxFit.cover
-            ),
-            // Image(
-            //   image: NetworkImage('https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg')
-            // ),
-            Container(
+              fit: BoxFit.cover),
+          // Image(
+          //   image: NetworkImage('https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg')
+          // ),
+          Container(
               padding: EdgeInsets.all(10.0),
-              child: Text('No tengo idea de que poner')
-              )
-          ],
-        ),
+              child: Text('No tengo idea de que poner'))
+        ],
+      ),
+      
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.5,
+            spreadRadius: 3.0,
+            // offset: Offset(10, 10)
+          )
+        ],
+        color: Colors.white
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card
+      ), 
     );
   }
 }
